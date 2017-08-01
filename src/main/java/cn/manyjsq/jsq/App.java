0b873ab7feed2a606e968e47107cf84ac0eed89b
yclv <lvyanchi@foxmail.com>
2017-08-01 17:08:32 +0800

    初始化项目

---
+++ src/main/java/cn/manyjsq/jsq/App.java
@@ -0,0 +1,13 @@
+package cn.manyjsq.jsq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
